027274df2da4640220208135b69d8c228da4d170
George Amoah <amsoundz451@gmail.com>
2023-10-22 14:23:49 -0400

    awwith user data

--- main.tf
+++ main.tf
@@ -9,3 +9,10 @@ resource "aws_lightsail_instance" "gitlab_test" {
     foo = "bar"
   }
 }
+
+resource "aws_lightsail_instance" "custom" {
+  name              = "custom"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_1_0"
+  user_data         = "sudo yum install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Terraform</h1>' | sudo tee /var/www/html/index.html"
